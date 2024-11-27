0172183255501b9825c3bc61ab572f9a201a62d3
UjjvalDhakad12 <allinonefan9@gmail.com>
2024-11-27 20:06:58 +0530

    ud site

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Home from './Home'
+import Navbar from './Components/Navbar'
+
+function App() {
+
+  return (
+    <div>
+      <Navbar />
+      <Home /> {/*Main page Home.jsx*/}
+    </div>
+  )
+}
+
+export default App
